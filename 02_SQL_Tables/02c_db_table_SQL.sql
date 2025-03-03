d6f3e5c18b0db37f1505fa56d48b90a122d2a086
“max” <max.wetstein@gmx.de>
2025-03-03 12:29:38 +0100

    neu

---
+++ 02_SQL_Tables/02c_db_table_SQL.sql
@@ -0,0 +1,49 @@
+\! cls
+/* ------  Strukturen ----- */
+
+/* 	 
+	Doppelte DS werden nicht mehr zugelassen
+    UNIQUE
+*/
+
+
+
+
+
+/* Datenbanken auf Server anzeigen */
+SHOW DATABASES;
+
+
+/* DB boo löschen, falls vorhanden*/
+DROP DATABASE IF EXISTS boo;
+
+/* DB boo anlegen, falls noch nicht vorhanden*/
+CREATE DATABASE IF NOT EXISTS boo;
+
+
+/* DB auswählen */
+USE boo;
+
+/* Tabelle anlegen */
+CREATE TABLE ichbineinetabelle
+(
+   name VARCHAR(20),
+   age INT
+
+);
+
+
+/* Alle Tabellen in der DB anzeigen */
+SHOW TABLES;
+-- SHOW TABLES FROM mysql;
+
+/* Struktur der Tabelle anzeigen */
+DESCRIBE ichbineinetabelle;
+
+/* ----- Daten ------- */
+INSERT INTO ichbineinetabelle(name,age) VALUES("Hans",35);
+INSERT INTO ichbineinetabelle(age,name) VALUES(29,"Gandalf");
+INSERT INTO ichbineinetabelle VALUES();
+
+/* ---- Inhalte der Tabelle anzeigen ---- */
+SELECT * FROM ichbineinetabelle;
